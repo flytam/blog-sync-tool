691f20077a3218b716420cc238c0fdd94133371f
flytam <tanjiahui2015@foxmail.com>
2022-10-02 10:42:47 +0800

    feat: add demo

---
+++ online-demo/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return <div className={styles.container}>1</div>
+}
+
+export default Home
